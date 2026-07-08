700866ca86daf09b6dde8fa611882accdf876273
KymCat <kohina1452@gmail.com>
2026-07-08 18:07:04 +0900

    게시글 수정 로직 변경 : Patch API 에 맞게 수정

--- src/main/java/com/example/blogStudy/dto/update/PostUpdate.java
+++ src/main/java/com/example/blogStudy/dto/update/PostUpdate.java
@@ -9,11 +9,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PostUpdate {
 
-    @NotBlank(message = "제목을 입력해주세요.")
     @Size(max = 100, message = "제목은 100자를 초과해서 입력하실 수 없습니다.")
     private String title;
 
-    @NotBlank(message = "내용을 입력해주세요.")
     @Size(max = 6000, message = "본문은 6000자를 초과해서 입력하실 수 없습니다.")
     private String content;
 
--- src/main/java/com/example/blogStudy/service/PostService.java
+++ src/main/java/com/example/blogStudy/service/PostService.java
@@ -59,6 +59,8 @@ public class PostService {
     // 게시글 수정
     @Transactional
     public PostResponse updatePost(String userId, Long id, PostUpdate dto) {
+        validatePostUpdate(dto);
+
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
@@ -79,6 +81,24 @@ public class PostService {
             throw new CustomException(ErrorCode.POST_ACCESS_DENIED);
 
         postRepository.delete(post);
+    }
+
+    /*
+        필드 전체 Null
+        필드 한쪽 공백,빈 문자열 검증 함수
+     */
+    private void validatePostUpdate(PostUpdate dto) {
+        if (dto.getTitle() == null && dto.getContent() == null) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+
+        if (dto.getTitle() != null && dto.getTitle().isBlank()) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+
+        if (dto.getContent() != null && dto.getContent().isBlank()) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
     }
 
 }
