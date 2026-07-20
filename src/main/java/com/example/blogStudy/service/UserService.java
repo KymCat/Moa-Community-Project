1b40e346786419d2ef64c0d0165b8d2a3b351a3d
KymCat <kohina1452@gmail.com>
2026-07-20 18:10:00 +0900

    DELETE "/users/me" 에서도 admin 유저 삭제 방지 코드 추가

--- src/main/java/com/example/blogStudy/service/UserService.java
+++ src/main/java/com/example/blogStudy/service/UserService.java
@@ -100,6 +100,9 @@ public class UserService {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
+        if (user.getRole() == Role.ADMIN){
+            throw new CustomException(ErrorCode.CANNOT_DELETE_ADMIN);
+        }
         userRepository.delete(user);
     }
 
