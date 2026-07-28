65acf53acb4b054e39451206765b96a24c287f92
KymCat <kohina1452@gmail.com>
2026-07-28 17:17:09 +0900

    Push API gradle, 엔티티 작성완료

---
+++ src/main/java/com/example/blogStudy/entity/Notification.java
@@ -0,0 +1,74 @@
+package com.example.blogStudy.entity;
+
+import com.example.blogStudy.entity.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "notification",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uq_notification_comment_id",    // 댓글 하나당 알림 한개
+                        columnNames = "comment_id"
+                )
+        },
+        indexes = {
+                @Index(
+                        name = "idx_notification_receiver_created", // 알림순 조회 대비
+                        columnList = "receiver_id, created_at"
+                ),
+                @Index(
+                        name = "idx_notification_receiver_read", // 읽지 않은 알림개수 조회 대비
+                        columnList = "receiver_id, read_at"
+                )
+        }
+
+)
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "read_at", nullable = false)
+    private Instant readAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    public static Notification create(User user, Comment comment) {
+        Notification nf = new Notification();
+        nf.user = user;
+        nf.comment = comment;
+
+        return nf;
+    }
+
+    public boolean isRead() {
+        return readAt != null;
+    }
+
+    // 읽음 표시
+    public void markAsRead() {
+        if (readAt == null)
+            readAt = Instant.now();
+    }
+
+}
---
+++ src/main/java/com/example/blogStudy/entity/PushSubscription.java
@@ -0,0 +1,73 @@
+package com.example.blogStudy.entity;
+
+import com.example.blogStudy.entity.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "pushSubscription",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uq_push_subscription_endpoint_hash",    // 구독 중복 방지, endpoint 조회
+                        columnNames = "endpoint_hash"
+                )
+        },
+        indexes = {
+                @Index(
+                        name = "idx_push_subscription_user_id", // 사용자의 구독 전체 조회 대비
+                        columnList = "user_id"
+                )
+        }
+)
+public class PushSubscription  extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(nullable = false)
+    private String endpoint;
+
+    @Column(name = "endpoint_hash", nullable = false, length = 64)
+    private String endpointHash;
+
+    @Column(nullable = false, length = 128)
+    private String p256dh;
+
+    @Column(nullable = false, length = 64)
+    private String auth;
+
+    // Fk
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public static PushSubscription create(
+            User user,
+            String endpoint,
+            String endpointHash,
+            String p256dh,
+            String auth
+    ) {
+       PushSubscription ps = new PushSubscription();
+       ps.user = user;
+       ps.endpoint = endpoint;
+       ps.endpointHash = endpointHash;
+       ps.p256dh = p256dh;
+       ps.auth = auth;
+
+        return ps;
+    }
+
+    public void update(User user, String p256dh, String auth) {
+        this.user = user;
+        this.p256dh = p256dh;
+        this.auth = auth;
+    }
+}
